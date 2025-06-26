362d45656460cba60a95402d94e03a83cc9d3dcf
dt313 <dt313.dev21@gmail.com>
2025-06-27 00:23:12 +0900

    feat: Add voice call feature - websocket

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import server from './app';
 import config from './configs/config';
 
+// // change on production
 server.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
 });
